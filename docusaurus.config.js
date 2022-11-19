27db2cf990a5c64a4be811fdcbb65eef5fc6bba8
Esteban <steban@gmail.com>
2022-11-19 18:22:22 -0500

    123

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -92,14 +92,7 @@ const config = {
           {
             title: 'Community',
             items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
+              
               {
                 label: 'Twitter',
                 href: 'https://twitter.com/docusaurus',
