39933e08373a75aa16ade3dce4dfdf6b6d377496
Esteban <steban@gmail.com>
2022-11-19 18:24:31 -0500

    123

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -71,7 +71,7 @@ const config = {
           // },
           // {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/facebook/docusaurus',
+            href: 'https://github.com/evm20/',
             label: 'GitHub',
             position: 'right',
           },
