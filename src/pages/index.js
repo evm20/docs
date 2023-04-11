86ead02430533e9ebf3c641d7fc3a8fe541c091b
Esteban Saá <steban@gmail.com>
2023-04-10 19:43:39 -0500

    name change

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            TrustSwap Introduction - 5min ⏱️
+            Frogge.finance Introduction - 5min ⏱️
           </Link>
         </div>
       </div>
