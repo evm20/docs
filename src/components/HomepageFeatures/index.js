dab6f7d216ee6ac54b5d9b145f50890d92142647
Esteban Saá <steban@gmail.com>
2023-05-03 13:41:28 -0500

    fix

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -32,11 +32,7 @@ export default function HomepageFeatures() {
   return (
     <section className={styles.features}>
       <div className="container">
-        <div className="row">
-          {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
-          ))}
-        </div>
+       
       </div>
     </section>
   );
