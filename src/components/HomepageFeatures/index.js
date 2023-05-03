f195f295a972b558dd07fe41e257468d74e1ddc8
Esteban Saá <steban@gmail.com>
2023-05-03 12:13:46 -0500

    some updates

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -2,37 +2,17 @@ import React from 'react';
 import clsx from 'clsx';
 import styles from './styles.module.css';
 
-const FeatureList = [
-  {
-    title: 'The first swap on EOS EVM',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
-    description: (
-      <>
-        We are just getting started...
-      </>
-    ),
-  },
-  // {
-  //   title: 'Focus on What Matters',
-  //   Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
-  //   description: (
-  //     <>
-  //       Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-  //       ahead and move your docs into the <code>docs</code> directory.
-  //     </>
-  //   ),
-  // },
-  // {
-  //   title: 'Powered by React',
-  //   Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
-  //   description: (
-  //     <>
-  //       Extend or customize your website layout by reusing React. Docusaurus can
-  //       be extended while reusing the same header and footer.
-  //     </>
-  //   ),
-  // },
-];
+//const FeatureList = [
+//  {
+//    title: 'The first swap on EOS EVM',
+//    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+//    description: (
+//     <>
+//        We are just getting started...
+//      </>
+//   ),
+//  },
+// ];
 
 function Feature({Svg, title, description}) {
   return (
